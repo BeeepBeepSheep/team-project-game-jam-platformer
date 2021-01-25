6b29ed218b6553f891a9031ee498e8e7392b06d8
DESKTOP-PBD6PON\jacob <jacoblueke@gmail.com>
2021-01-25 11:31:26 +0000

    og commit with main menu

---
+++ Assets/sceneloader1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneloader1 : MonoBehaviour
+{
+    public void LoadGame1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
